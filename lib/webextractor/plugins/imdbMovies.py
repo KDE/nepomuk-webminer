fa48051c50191a1f62747ce72cfb2411b87828a5
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2012-03-07 22:50:20 +0100

    added iserantes diff

--- lib/webextractor/plugins/imdbMovies.py
+++ lib/webextractor/plugins/imdbMovies.py
@@ -27,7 +27,6 @@ def info():
     return dict( name = 'The Internet Movie Database',
                  icon = 'imdb.png',
                  identifier = 'imdbmovies',
-                 urlregex = 'http://www.imdb.com/title/tt',
                  resource = 'movie',
                  description = 'some description',
                  author = 'Joerg Ehrichs',
@@ -46,14 +45,14 @@ def info():
 # WebExtractor.searchResultsInfo( dict() )
 #
 def searchItems( parameters ):
-	
+
 	title = parameters['title']
 	yearMin = parameters['yearMin']
 	yearMax = parameters['yearMax']
 
 	ia = IMDb()
 	results = ia.search_movie(title)
-	
+
 	searchResults = []
 	for item in results :
                 itemKind = str(item['kind'])
@@ -70,7 +69,7 @@ def searchItems( parameters ):
                                         )
 
                         searchResults.append(entryDict)
-	
+
 	WebExtractor.searchResults( searchResults )
 
 
@@ -80,31 +79,32 @@ def extractItemFromUri( url ):
 	if not match:
 		print 'Error getting movie id from url'
 	movieId = match.group(1)
-	
+
 	ia = IMDb()
 	movie = ia.get_movie(movieId)
-	
-	directorString = ''
-	for director in movie['director']:
-		directorString = directorString + ';' + director['name']
 
-	writerString = ''
-	for writer in movie['writer']:
-		writerString = writerString + ';' + writer['name']
+        # We can solve the ; bug at the beggining and improve the code a little bit ;).
+	#directorString = ''
+	#for director in movie['director']:
+	#	directorString = directorString + ';' + director['name']
 
-	castString = ''
-	for cast in movie['cast']:
-		castString = castString + ';' + cast['name']
+	#writerString = ''
+	#for writer in movie['writer']:
+	#	writerString = writerString + ';' + writer['name']
 
-	result = dict(  title = movie['title'],
-			year = movie['year'],
-			director = directorString,
-			writer = writerString,
-			cast = castString,
-			genres = ';'.join(movie['genres']),
-			plot = ';'.join(movie['plot']),
-			runtime = ';'.join(movie['runtimes']),
-			countries = ';'.join(movie['countries'])
-			)
-	
+	#castString = ''
+	#for cast in movie['cast']:
+	#	castString = castString + ';' + cast['name']
+
+        result = dict(  title = movie['title'],
+                        year = movie['year'],
+                        director = ";".join([item["name"] for item in movie['director']]),
+                        writer = ";".join([item["name"] for item in movie['writer']]),
+                        cast = ";".join([item["name"] for item in movie['cast']]),
+                        genres = ';'.join(movie['genres']),
+                        plot = ';'.join(movie['plot']),
+                        runtime = ';'.join(movie['runtimes']),
+                        countries = ';'.join(movie['countries'])
+                        )
+
 	WebExtractor.itemResults( result )
