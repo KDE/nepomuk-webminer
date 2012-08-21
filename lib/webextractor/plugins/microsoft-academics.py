2cd3825a3cada8a3973c21d5fcb864c90bfccd5b
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2012-08-22 01:34:07 +0200

    simplyfy msa plugin

--- lib/webextractor/plugins/microsoft-academics.py
+++ lib/webextractor/plugins/microsoft-academics.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env kross
 
-import Kross
-from PyQt4 import QtCore
-import PyKDE4.kdecore as kdecore
-from PyKDE4.kio import KIO
-
 import json
-import urllib
 import re
 
 # Prerrequisites.
-isAvailable = False
+isAvailable = True
 errorMsg = ""
 try:
-    from BeautifulSoup import BeautifulSoup # note switch to lxml
+    import string
+    import urllib2
+    import httplib2
+    from BeautifulSoup import BeautifulSoup
 
 except:
     isAvailable = False
-    errorMsg = "The BeautifulSoup python module needs to be installed."
+    errorMsg = "Module requirements missing. springer.py needs:\nThe BeautifulSoup python module. see: http://www.crummy.com/software/BeautifulSoup/\nThe Httplib2 python module. See: http://code.google.com/p/httplib2/"
 
 
-finalEntry = {}
 masid = ''
 citations = []
 citationResults = []
@@ -29,7 +25,7 @@ citationResults = []
 # Module options
 
 # Must have:
-# name          = Long name of teh service
+# name          = Long name of the service
 # icon          = download the favicon and put is as png in the folder, icon is than name.png
 # identifier    = short unique identifier for the service, no special chars, whitespaces
 # resource      = must be either publication, tvshow or movie. Describes what kind of data this plugin fetches
@@ -86,37 +82,15 @@ def searchItems( resourcetype, parameters ):
     if (journal is not None) and (journal):
         queryPart += ' jour:(' + journal + ')'
 
-    query = "http://academic.research.microsoft.com/Search?query=" + queryPart + '&start=0&end=100'
+    query = "http://academic.research.microsoft.com/Search?query=" + urllib2.quote(queryPart.encode("utf8")) + '&start=0&end=50'
 
     logMsg = 'start search query at: ' + query 
     WebExtractor.log( logMsg )
 
-    # KIO part to fetch asyncron 
-    data_url = kdecore.KUrl( query )
-    retrieve_job = KIO.storedGet(data_url, KIO.NoReload, KIO.HideProgressInfo)
-    retrieve_job.result.connect(handleSearchQuery)
+    h = httplib2.Http(".cache")
+    resp, content = h.request(query)
 
-#------------------------------------------------------------------------------
-# process the returned search results from the KIO call
-#
-# calls extractSearchResults to extract the items or
-# emits the error signal if something happend
-#
-def handleSearchQuery(job):
-
-    if job.error():
-        error = 'kio job error occured'
-        WebExtractor.error( error )
-        return
-
-    data = job.data()
-
-    #import os
-    #gSysEncoding = os.environ["LANG"].split(".")[1]
-    html = unicode(str(QtCore.QString(data).toUtf8()), 'utf-8')
-    #print QtCore.QString(data).toUtf8()
-
-    documentElement = BeautifulSoup( html ) # note switch to lxml
+    documentElement = BeautifulSoup( content )
 
     # parse page to retrive all item data
     searchResults = extractSearchResults(documentElement, False)
@@ -147,12 +121,14 @@ def extractSearchResults(documentElement, citation=False):
         WebExtractor.error( 'Could not extract Item info' )
         return searchResults
 
+
     if citation is True:
         i = 0
     else:
         i = 1
 
-    for child in documentElement.findAll('li', {'class':'paper-item'}):
+    pubItems = documentElement.findAll('li', 'paper-item')
+    for child in pubItems:
         if(i < 10):
             fetchentry = '0' + str(i)
         else:
@@ -172,7 +148,7 @@ def extractSearchResults(documentElement, citation=False):
             exp = re.compile(r'Publication/(\d+)/')
             match = exp.search(linkPart)
             if not match:
-                print 'Error parsing publication masid'
+                WebExtractor.error( 'Error parsing publication masid' )
             masid = match.group(1)
 
             fullUrl = 'http://academic.research.microsoft.com/Publication/' + masid
@@ -201,8 +177,8 @@ def extractSearchResults(documentElement, citation=False):
 #------------------------------------------------------------------------------
 # extracts the information of one item from the page at the url location
 #
-# in here it calls either the extarctItem function that loads the citations too or
-# just the item without further citation information
+# If the options allow to fetch citations (which is very slow as it might need to fetch lots of them) we call fetchNextCitation() for each 
+# citation element available
 #
 # when all information are retrieved a call via:
 # WebExtractor.itemResults( 'publication', dict() )
@@ -213,56 +189,57 @@ def extractSearchResults(documentElement, citation=False):
 # bibtexentrytype =...          like book, article, inproceedings // necessary for the PublicationPipe to work properly
 # references = ...              list of dicts, where each dict is another publication dict
 #
-# TODO: implement option to allow / disallow reference fetching in handleMainItemExtraction, refernce fetching takes a lot of time
 def extractItemFromUri( url, options ):
 
     logMsg = 'start item extraction via: ' + url 
     WebExtractor.log( logMsg )
 
-    global fetchCitations
     fetchCitations = options['references']
 
-    # KIO part to fetch asyncron 
-    data_url = kdecore.KUrl( url )
-    retrieve_job = KIO.storedGet(data_url, KIO.NoReload, KIO.HideProgressInfo)
-    retrieve_job.result.connect(handleMainItemExtraction)
+    #clear all citations available from an earlier call
+    global citations
+    citations = []
+    global citations
+    citationResults = []
 
-#------------------------------------------------------------------------------
-# process the returned item results from the KIO call
-#
-# calls extractItem to extract the items or
-# emits the error signal if kio failed to fetch the data
-# 
-# and fetches the page with the possible reference list we need to process them
-def handleMainItemExtraction(job):
+    h = httplib2.Http(".cache")
+    resp, content = h.request(url)
 
-    if job.error():
-        error = 'kio job error occured'
-        WebExtractor.error( error )
-        return
-
-    data = job.data()
-
-    html = unicode(str(QtCore.QString(data).toUtf8()), 'utf-8')
-
-    documentElement = BeautifulSoup( html ) # note switch to lxml
+    documentElement = BeautifulSoup( content )
 
     # retrieve the basic data
-    global finalEntry
     finalEntry = extractItem( documentElement )
 
     if fetchCitations:
         # check if we can fetch references
+        # masid is a global variable retrieved in extractItem() 
         url = 'http://academic.research.microsoft.com/Detail?entitytype=1&searchtype=5&id=' + masid + '&start=1&end=100'
         logMsg = 'fetch references via ' + url 
         WebExtractor.log( logMsg )
-        # KIO part to fetch asyncron 
-        data_url = kdecore.KUrl( url )
-        retrieve_job = KIO.storedGet(data_url, KIO.NoReload, KIO.HideProgressInfo)
-        retrieve_job.result.connect(handleCitationSearch)
-    else:
-        data_string = json.dumps(finalEntry)
-        WebExtractor.itemResultsJSON( 'publication', data_string )
+
+        h = httplib2.Http(".cache")
+        resp, content = h.request(url)
+
+        documentElement = BeautifulSoup( content )
+
+        global citations
+        citations = extractSearchResults(documentElement, True)
+
+        if not citations:
+            WebExtractor.log( 'No references found' )
+
+        while citations:
+            fetchNextCitation()
+
+        # Add all found citations to the final entry
+        global citationResults
+        if citationResults:
+            finalEntry['references'] = citationResults
+
+
+    data_string = json.dumps(finalEntry)
+    WebExtractor.itemResultsJSON( 'publication', data_string )
+
 
 #------------------------------------------------------------------------------
 # does the dirty work of extracting the html data
@@ -286,20 +263,22 @@ def extractItem( documentElement ):
         WebExtractor.error( 'Could not fetch masid.' )
         return entry
 
-    logMsg = 'masid found: ' + masid 
+    logMsg = 'masid found: ' + masid
     WebExtractor.log( logMsg )
 
     # 2. we get propper publication data, this is available as bibtex file download
     # http://academic.research.microsoft.com/64764.bib?type=2&format=0&download=1
-    # TODO:replace urllib by async appraoch
     WebExtractor.log( 'download bibtex file' )
-    filehandle = urllib.urlopen('http://academic.research.microsoft.com/' + masid + '.bib?type=2&format=0&download=1')
+
+    h = httplib2.Http(".cache")
+    resp, content = h.request( 'http://academic.research.microsoft.com/' + masid + '.bib?type=2&format=0&download=1' )
 
     bibtexentrytype = re.compile(r'^@(\w+)\{(.*)')   # works on            :: @article{author = {Hans Wurst},
     bibtexKeys = re.compile(r'^(\w+)\s=\s\{+([^\{\}]*)\}+,') # works on any entry :: key = {value},
 
     # transform bibtex keys 1:1 into the dictionary we want to return
-    for line in filehandle.readlines():
+    for line in string.split(content, "\n"):
+        pp.pprint(line)
         typeMatch = bibtexentrytype.search(line)
         if(typeMatch):
             entry.update(dict(bibtexentrytype = typeMatch.group(1)))
@@ -312,8 +291,6 @@ def extractItem( documentElement ):
         if(keysMatch):
             entry.update( {keysMatch.group(1) : keysMatch.group(2)})
 
-    filehandle.close()
-
     # great, but there is more: fetch the abstract from the page
     abstract = documentElement.first('span', {'id':'ctl00_MainContent_PaperItem_snippet'})
     if abstract is not None:
@@ -322,38 +299,6 @@ def extractItem( documentElement ):
     # TODO: parse downloadable files ... so we get informations on where to get the pdf file and so on
 
     return entry
-
-#------------------------------------------------------------------------------
-# Like the handleMainItemExtraction this processes the page where all references are listed as returned from the KIO call
-#
-# calls extractSearchResults tif we found a valid list to get the proper item info for each item
-# if citations are found fetchNextCitation keeps track of fetching the next citation item until we are finished
-# 
-def handleCitationSearch(job):
-
-    if job.error():
-        error = 'kio job error occured'
-        WebExtractor.error( error )
-        return
-
-    data = job.data()
-
-    html = unicode(str(QtCore.QString(data).toUtf8()), 'utf-8')
-
-    documentElement = BeautifulSoup( html ) # note switch to lxml
-
-    global citations
-    citations = extractSearchResults(documentElement, True)
-
-    if citations:
-        fetchNextCitation()
-    else:
-        WebExtractor.log( 'No references found' )
-        #WebExtractor.itemResults( 'publication', finalEntry )
-        # Use JSON was a workaround of an multiple nested dict issue with KROSS
-        data_string = json.dumps(finalEntry)
-        WebExtractor.itemResultsJSON( 'publication', data_string )
-
 
 #------------------------------------------------------------------------------
 #  checks the global "citations" if more citation etries are available
@@ -366,53 +311,22 @@ def fetchNextCitation():
     nextCitation = citations.pop()
 
     # check if we can fetch references
-    logMsg = 'fetch reference' +  nextCitation['title']
+    logMsg = 'fetch reference: ' +  nextCitation['title']
     WebExtractor.log( logMsg )
 
-    # KIO part to fetch asyncron 
-    data_url = kdecore.KUrl( nextCitation['url'] )
-    retrieve_job = KIO.storedGet(data_url, KIO.NoReload, KIO.HideProgressInfo)
-    retrieve_job.result.connect(handleCitationItemExtraction)
+    h = httplib2.Http(".cache")
+    resp, content = h.request(nextCitation['url'])
 
-#------------------------------------------------------------------------------
-#  Process the ite mdata for a single citation element
-#
-# does not fetch any citation of this, to avoid a never endig loop where hundrets of citations are fetched
-#
-def handleCitationItemExtraction(job):
-
-    global finalEntry
-    if job.error():
-        error = 'kio job error occured'
-        WebExtractor.error( error )
-        #WebExtractor.itemResults( 'publication', finalEntry )
-        data_string = json.dumps(finalEntry)
-        WebExtractor.itemResultsJSON( 'publication', data_string )
-        return
-
-    data = job.data()
-
-    html = unicode(str(QtCore.QString(data).toUtf8()), 'utf-8')
-
-    documentElement = BeautifulSoup( html ) # note switch to lxml
+    documentElement = BeautifulSoup( content )
 
     # retrieve the basic data
-    global citations
     citation = extractItem( documentElement )
-
-    if citation:
-        WebExtractor.log( 'citation fetched' )
 
     global citationResults
     citationResults.append( citation )
 
-    if citations:
-        fetchNextCitation()
-    else:
-        finalEntry['references'] = citationResults
-        #WebExtractor.itemResults( 'publication', finalEntry )
-
-        # this is a workaround, because multiple nested dictionaries cause some problems with the names of the keys
-        data_string = json.dumps(finalEntry)
-        WebExtractor.itemResultsJSON( 'publication', data_string )
-
+if __name__=="__main__":
+    print "Plugin information:"
+    pluginInfo = info()
+    for key in pluginInfo:
+        print "%15s: %s" % (key, pluginInfo[key])
