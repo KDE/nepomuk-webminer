406ab2119685517258952d3eb38708bcbafb169d
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2012-09-10 13:57:28 +0200

    add BatchExtractor. Queued background data fetching/pipeing

---
+++ lib/ui/batchextractor.cpp
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2012 Jörg Ehrichs <joerg.ehrichs@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "batchextractor.h"
+
+#include "webextractor/extractorfactory.h"
+#include "webextractor/webextractor.h"
+
+#include "nepomukpipe/nepomukpipe.h"
+#include "nepomukpipe/moviepipe.h"
+#include "nepomukpipe/musicpipe.h"
+#include "nepomukpipe/publicationpipe.h"
+#include "nepomukpipe/tvshowpipe.h"
+
+#include <KDE/KDebug>
+
+#include <QtCore/QList>
+#include <QtCore/QFutureWatcher>
+#include <QtCore/QtConcurrentRun>
+
+namespace NepomukMetaDataExtractor {
+namespace UI {
+    class BatchExtractorPrivate {
+    public:
+        NepomukMetaDataExtractor::Extractor::ExtractorFactory extractorFactory;
+        NepomukMetaDataExtractor::Extractor::WebExtractor *currentExtractor;
+        QList<NepomukMetaDataExtractor::UI::BatchExtractor::ExtractionJob> jobList;
+        bool extractionInProgress;
+    };
+}
+}
+
+using namespace NepomukMetaDataExtractor::Pipe;
+using namespace NepomukMetaDataExtractor::Extractor;
+using namespace NepomukMetaDataExtractor::UI;
+
+NepomukMetaDataExtractor::UI::BatchExtractor::BatchExtractor(QObject *parent)
+    : QObject(parent)
+    , d_ptr( new NepomukMetaDataExtractor::UI::BatchExtractorPrivate )
+{
+    Q_D( BatchExtractor );
+
+    d->extractionInProgress = false;
+    d->currentExtractor = 0;
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::addJob(const QUrl &detailsUrl, const QVariantMap &options, const QString &name, const QString &resourceUri )
+{
+    Q_D( BatchExtractor );
+
+    kDebug() << "addJob running?" << d->extractionInProgress;
+
+    ExtractionJob newJob;
+    newJob.detailsUrl = detailsUrl;
+    newJob.options = options;
+    newJob.name = name;
+    newJob.resourceUri = resourceUri;
+
+    d->jobList.append( newJob );
+
+    if( !d->extractionInProgress ) {
+        extractNext();
+    }
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::extractNext()
+{
+    Q_D( BatchExtractor );
+    d->extractionInProgress = true;
+
+    ExtractionJob nextJob = d->jobList.first();
+
+    if( d->currentExtractor ) {
+        disconnect( d->currentExtractor, SIGNAL(error(QString)), this, SLOT(error(QString)) );
+        disconnect( d->currentExtractor, SIGNAL(log(QString)), this, SLOT(log(QString)) );
+        disconnect( d->currentExtractor, SIGNAL(itemResults(QString,QVariantMap)), this, SLOT(itemResults(QString,QVariantMap)) );
+    }
+
+    d->currentExtractor = d->extractorFactory.getExtractor(nextJob.detailsUrl);
+
+    connect( d->currentExtractor, SIGNAL(error(QString)), this, SLOT(error(QString)) );
+    connect( d->currentExtractor, SIGNAL(log(QString)), this, SLOT(log(QString)) );
+
+    connect( d->currentExtractor, SIGNAL(itemResults(QString,QVariantMap)), this, SLOT(itemResults(QString,QVariantMap)) );
+
+    d->currentExtractor->extractItem( nextJob.detailsUrl, nextJob.options);
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::log(const QString &msg)
+{
+    kDebug() << msg;
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::error(const QString &msg)
+{
+    kDebug() << msg;
+}
+
+static void concurrentPipe(const QString &resourceType, const QVariantMap &entry)
+{
+    // find the right nepomuk pipe and throw it in
+    NepomukPipe *nepomukPipe = 0;
+
+    if(resourceType == QLatin1String("publication")) {
+        nepomukPipe = new PublicationPipe;
+    }
+    else if(resourceType == QLatin1String("tvshow")) {
+        nepomukPipe = new TvShowPipe;
+    }
+    else if(resourceType == QLatin1String("movie")) {
+        nepomukPipe = new MoviePipe;
+    }
+    else if(resourceType == QLatin1String("music")) {
+        nepomukPipe = new MusicPipe;
+    }
+
+    if(nepomukPipe) {
+        nepomukPipe->pipeImport( entry );
+    }
+    else {
+        kWarning() << "No nepomuk pipe available for the resource type" << resourceType;
+    }
+
+    delete nepomukPipe;
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::itemResults(const QString &resourceType, const QVariantMap &entry)
+{
+    Q_D( BatchExtractor );
+
+    ExtractionJob nextJob = d->jobList.first();
+
+    QVariantMap metaData = entry;
+    if( !nextJob.resourceUri.isEmpty() ) {
+        addResourceUriToMetaData(resourceType, nextJob.resourceUri, metaData);
+    }
+
+    QFuture<void> future = QtConcurrent::run(concurrentPipe, resourceType, metaData);
+    QFutureWatcher<void> *futureWatcher = new QFutureWatcher<void>();
+
+    connect( futureWatcher, SIGNAL(finished()), this, SLOT(pipeFinished()) );
+
+    futureWatcher->setFuture(future);
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::addResourceUriToMetaData( const QString &resourceType, const QString &uri, QVariantMap &metadata )
+{
+    // For tv shows put the resource uri into the Episode part of the MetaData
+    // this way around it is possible to use the TvShowPipe with more episode/files at once
+    if( resourceType == QLatin1String("tvshow")) {
+        QVariantList seasons = metadata.value(QLatin1String("seasons")).toList();
+        if(!seasons.isEmpty()) {
+            QVariantMap season = seasons.takeFirst().toMap();
+            QVariantList episodes = season.value(QLatin1String("episodes")).toList();
+
+            if(!episodes.isEmpty()) {
+                QVariantMap episodesMap = episodes.takeFirst().toMap();
+                kDebug() << "add to episode" << episodesMap.value(QLatin1String("title")).toString() << "url" << uri;
+                episodesMap.insert(QLatin1String("resourceuri"), uri);
+
+                episodes << episodesMap;
+                season.insert( QLatin1String("episodes"), episodes);
+                seasons << season;
+                metadata.insert( QLatin1String("seasons"), seasons);
+            }
+        }
+    }
+    // music piece / album works the same as tvshows, we add the fileurl to the track not the toplevel album
+    else if( resourceType == QLatin1String("music")) {
+        QVariantList trackList = metadata.value(QLatin1String("tracks")).toList();
+
+        if(!trackList.isEmpty()) {
+            QVariantMap trackMap = trackList.takeFirst().toMap();
+            kDebug() << "add to track" << trackMap.value(QLatin1String("title")).toString() << "url" << uri;
+            trackMap.insert(QLatin1String("resourceuri"), uri);
+
+            trackList << trackMap;
+            metadata.insert( QLatin1String("tracks"), trackList);
+        }
+    }
+
+    // For all other cases put the resource uri in the top part of the MetaData
+    // add this for all cases, helps to find the folder for the poster/cover download easier for music/tvshows)
+    metadata.insert(QLatin1String("resourceuri"), uri);
+}
+
+void NepomukMetaDataExtractor::UI::BatchExtractor::pipeFinished()
+{
+    sender()->deleteLater(); // delete the created future watcher again
+
+    Q_D( BatchExtractor );
+
+    kDebug() << "##############################################################";
+    d->jobList.takeFirst();
+
+    // parallel fetch from the web and pipe the last to nepomuk
+    if( !d->jobList.isEmpty() ) {
+        extractNext();
+    }
+    else {
+        d->extractionInProgress = false;
+    }
+}
+
---
+++ lib/ui/batchextractor.h
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2012 Jörg Ehrichs <joerg.ehrichs@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BATCHEXTRACTOR_H
+#define BATCHEXTRACTOR_H
+
+#include <QtCore/QObject>
+
+#include <QtCore/QUrl>
+#include <QtCore/QVariantList>
+
+#include "nepomukmetadataextractor_export.h"
+
+namespace NepomukMetaDataExtractor {
+namespace UI {
+    class BatchExtractorPrivate;
+
+/**
+ * @brief The BatchExtractor will extract metadata and pipe it to nepomuk in the background
+ *
+ * Unlike the AutomaticFetcher this one operates on item detail urls only and does not search for
+ * the right website.
+ *
+ * This one can be used in external programs after the initial search for an item has been done.
+ * The details url with some options are handed to the BatchExtractor and will be downloaded and piped
+ * to nepomuk one by one in the background without interrupting the currentt UI workflow.
+ *
+ * The current status of the extraction job will be emitted via some signals to give the user some
+ * visual feedback whats happening in the background. Also the user can decide to cancel the job if
+ * necessary.
+ */
+class NEPOMUKMETADATAEXTRACTOR_EXPORT BatchExtractor : public QObject
+{
+    Q_OBJECT
+public:
+    /**
+     * @brief Holds the information for one extraction job object
+     */
+    struct ExtractionJob {
+        QUrl detailsUrl;        /**< the weburl where the metadata is extracted from */
+        QVariantMap options;    /**< some additional options for the extraction plugin */
+        QString name;           /**< a name that describes this job */
+        QString resourceUri;    /**< optional nepomuk resource uri if the data should be added to an existing resource (file for example) */
+    };
+
+    explicit BatchExtractor(QObject *parent = 0);
+
+public slots:
+    /**
+     * @brief Adds a new extraction job to the queue
+     *
+     * @param detailsUrl the weburl where the metadata is extracted from
+     * @param options some additional options for the extraction plugin
+     * @param name a name that describes this job
+     * @param resourceUri optional nepomuk resource uri if the data should be added to an existing resource (file for example)
+     */
+    void addJob(const QUrl &detailsUrl, const QVariantMap &options,
+                const QString &name, const QString &resourceUri = QString() );
+
+private:
+    /**
+     * @brief takes first entry in the queue and starts the extarction
+     */
+    void extractNext();
+
+    /**
+     * @brief helper function if metadata is added to an existing nepomuk resource
+     *
+     * Ensures the nepomuk uri is in the right part of the metadata variantmap
+     * @param resourceType resource type of the data
+     * @param uri the nepomuk resource uri
+     * @param metadata the metadata map
+     */
+    void addResourceUriToMetaData( const QString &resourceType, const QString &uri, QVariantMap &metadata );
+
+private slots:
+    /**
+     * @brief called from the Webextractor
+     * @param msg log message
+     */
+    void log(const QString &msg);
+
+    /**
+     * @brief called from the Webextractor
+     * @param msg error message
+     */
+    void error(const QString &msg);
+
+    /**
+     * @brief called from the Webextractor and returns all metadata
+     * @param resourceType resource type according to the plugin
+     * @param entry metadata entry from the plugin
+     */
+    void itemResults(const QString &resourceType, const QVariantMap &entry);
+
+    void pipeFinished();
+
+private:
+    Q_DECLARE_PRIVATE(BatchExtractor)
+    BatchExtractorPrivate *const d_ptr; /**< d-pointer for this class */
+};
+
+}
+}
+#endif // BATCHEXTRACTOR_H
