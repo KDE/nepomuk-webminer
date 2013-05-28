eb4367855ed9d3af995f74fc61cdc195bf1c87c3
Lim Yuen Hoe <yuenhoe@hotmail.com>
2013-05-29 00:59:48 +0800

    tvdbmal: refactor mal code and conf rule application code into their own classes

--- src/lib/webextractor/plugins/tvdbmal.py
+++ src/lib/webextractor/plugins/tvdbmal.py
@@ -42,10 +42,176 @@ except:
     errorMsg = "PyKDE is a requirement. Please install Python KDE bindings."
 
 
-confFileName = "tvdbrc"
 # possible FIXME:is there a better way than this hardcoding?
 uifileFolder = kdecore.KStandardDirs.locate("data", "nepomuk-webminer/plugins/") + "/uifiles";
 
+#------------------------------------------------------------------------------
+# A wrapper class that does the stuff we need with MAL: find aliases and deduce season numbers
+# Some of this code is adapted from malconstrict, a MAL api python wrapper
+class MalWrapper:
+    def __init__(self):
+        self.malapiurl = 'http://mal-api.com'
+        self.the_mal_entry = -1
+        self.cur_query = ""
+
+    def __raw_search_anime(self, query):
+        h = httplib2.Http("/tmp/httplib2")
+        resp, content = h.request(self.malapiurl + '/anime/search?q=' + urllib2.quote(query))
+        if int(resp['status']) != 200:
+            return None
+        return content
+
+    def __raw_get_anime_details(self, anime_id):
+        h = httplib2.Http("/tmp/httplib2")
+        resp, content = h.request(self.malapiurl + '/anime/' + str(anime_id) + '?mine=0')
+
+        if int(resp['status']) == 404:
+            return None
+        return content
+
+    # tries to find the "best match" series entry on MAL for given query
+    def get_mal_entry(self, query):
+        if self.the_mal_entry != -1 and self.cur_query == query:
+            return self.the_mal_entry;
+        self.cur_query = query
+        min_acceptable_score = 0.7
+        entries = json.loads(self.__raw_search_anime(query))
+        # put some back into finding the best result!
+        details = None
+        while len(entries) > 0:
+            # first we try and pick out the best title match
+            best_score = 0
+            best_match_index = 0
+            for i in range(len(entries)):
+                if self.__match_score(entries[i]['title'], query) > best_score:
+                    best_score = self.__match_score(entries[i]['title'], query)
+                    best_match_index = i
+            best_match = entries.pop(best_match_index)
+            if (best_match['type'] != "TV"):
+                continue # we ignore non TV-series for now
+            details = json.loads(self.__raw_get_anime_details(best_match['id']))
+            WebExtractor.log("Evaluating "+ details['title'])
+            # then we check if there is at least one alias meeting the minimum score
+            if (self.__match_score(details['title'], query) > min_acceptable_score):
+                break;
+            if 'english' in details['other_titles']:
+                for title in details['other_titles']['english']:
+                    if (self.__match_score(title, query) > min_acceptable_score):
+                        break;
+            if 'synonyms' in details['other_titles']:
+                for title in details['other_titles']['synonyms']:
+                    if (self.__match_score(title, query) > min_acceptable_score):
+                        break;
+            details = None
+        return details
+
+    # finds aliases
+    def get_other_titles(self, query):
+        details = self.get_mal_entry(query)
+        result = None
+        if details:
+            result = [details['title']]
+            if 'english' in details['other_titles']:
+                for title in details['other_titles']['english']:
+                    result.append(title)
+            if 'synonyms' in details['other_titles']:
+                for title in details['other_titles']['synonyms']:
+                    result.append(title)
+        return result
+
+    # well, deduces season number
+    def deduce_season(self, query, default):
+        details = self.get_mal_entry(query)
+        if details:
+            WebExtractor.log("Deducing season number with MAL..")
+            # firstly, a lot of shows list sequel shows as "<showname> Nth Season"
+            # in the synonym list, we look for that, and assume that "Nth Season"
+            # isn't actually part of the real title
+            # TODO: think harder about this assumption, can we avoid it?
+            if 'synonyms' in details['other_titles']:
+                for title in details['other_titles']['synonyms']:
+                    if title[-6:].lower() == "season":
+                        if title[-10:-9] == "1" and title[-9:-7].lower() == "st":
+                            WebExtractor.log("Deduced season with synonym " + title)
+                            return 1
+                        elif title[-10:-9] == "2" and title[-9:-7].lower() == "nd":
+                            WebExtractor.log("Deduced season with synonym " + title)
+                            return 2
+                        elif title[-10:-9].isdigit() and title[-9:-7].lower() == "th":
+                            WebExtractor.log("Deduced season with synonym " + title)
+                            return int(title[-10:-9])
+            # well that didn't work, guess we need to do it the hard way.
+            # basically, we see if a prequel is specified, and if it is, we look
+            # and see if the prequel has a prequel, and we deduce the season by
+            # counting the length of the prequel chain, assuming the end of the chain is
+            # season 1
+            season_count = 0
+            curentry = details
+            while True:
+                if curentry['type'] == "TV":
+                    WebExtractor.log("Prequel count at " + curentry['title'] + " : " + str(season_count))
+                    season_count += 1
+                if len(curentry['prequels']) < 1:
+                    break
+                curentry = json.loads(self.__raw_get_anime_details(curentry['prequels'][0]['anime_id']))
+            if season_count > 1:
+                return season_count
+        return default
+
+    def __match_score(self, str1, str2):
+        return difflib.SequenceMatcher(None, str1.lower(), str2.lower()).ratio()
+
+
+#------------------------------------------------------------------------------
+# Wrapper for config file and the rules therein
+#
+class ConfigRulesWrapper:
+    def __init__(self):
+        self.confFileName = "tvdbrc"
+        self.ruleSeparator = "|"
+        self.conf = kdecore.KConfig(self.confFileName)
+        self.confgroup = kdecore.KConfigGroup(self.conf, "aliases")
+
+    def apply_configured_rules(self, showtitle, showseason, showepisode):
+      
+        if showtitle is None or showtitle is '':
+            return showtitle, showseason, showepisode
+        
+        rawdata = self.confgroup.readEntry(showtitle, QtCore.QStringList()).toStringList()
+        if len(rawdata) > 1:
+            if rawdata[1] == "-":
+                return str(rawdata[0]), showseason, showepisode
+            else:
+                alias = rawdata.takeFirst()
+                season, episode = showseason, showepisode
+                for rule in rawdata:
+                    season, episode = self.apply_rule(rule, season, episode)
+                return str(alias), season, episode
+        # in case there's unicode involved
+        rawdata = self.confgroup.readEntry(showtitle.decode('utf'), QtCore.QStringList()).toStringList()
+        if len(rawdata) > 1:
+            if rawdata[1] == "-":
+                return str(rawdata[0]), showseason, showepisode
+            else:
+                alias = rawdata.takeFirst()
+                season, episode = showseason, showepisode
+                for rule in rawdata:
+                    season, episode = self.apply_rule(rule, season, episode)
+                return str(alias), season, episode
+        return showtitle, showseason, showepisode
+
+    def apply_rule(self, rule, showseason, showepisode):
+        components = QtCore.QString(rule).split(self.ruleSeparator)
+        if components.count() < 2:
+            return str(components[0]), showepisode
+        if showepisode >= str(components[1]) and (components[2] == "-" or showepisode <= str(components[2])) :
+            if components[3] == "1":
+                return str(components[0]), str(int(str(showepisode)) - int(str(components[1])) + 1)
+            else:
+                return str(components[0]), showepisode
+        return showseason, showepisode
+            
+        
 #------------------------------------------------------------------------------
 # Module options
 #
@@ -96,17 +262,19 @@ def info():
 # @todo add resourcetype 'series', 'season' to retrieve overview info of all episodes
 #       good for bulk download/select a folder and then fetch all episode info
 #
+malwrapper = MalWrapper()
+confwrapper = ConfigRulesWrapper()
 def searchItems( resourcetype, parameters ):
 
     t = tvdb_api.Tvdb()
 
-    searchResults = trySearch(parameters, t, "notfetched")
+    searchResults = trySearch(parameters, t)
     WebExtractor.searchResults( searchResults )
 
 #------------------------------------------------------------------------------
 # put the logic code here into a helper so we can retry recursively (for cases where we have aliases)
 
-def trySearch(parameters, t, the_mal_entry, recurse = True):
+def trySearch(parameters, t, recurse = True):
     
     title = ''
     showtitle = ''
@@ -125,14 +293,13 @@ def trySearch(parameters, t, the_mal_entry, recurse = True):
       
         WebExtractor.log("looking for configured aliases")
         oldseason = season
-        showtitle, season, episode = getConfiguredAlias(showtitle, season, episode)
-        if season == oldseason and season == "1":
+        showtitle, season, episode = confwrapper.apply_configured_rules(showtitle, season, episode)
+        if season == oldseason and season == "1" and recurse == True:
             # means we probably can't get a definite season from the filename or the alias config
             # so we make an effort to figure out the season ourself
             # Note we don't need to change the showtitle becauses tvdb seems to list sequel names searchably as well
-            if the_mal_entry == "notfetched":
-                the_mal_entry = get_mal_entry(showtitle)
-            season = mal_deduce_season(the_mal_entry, season)
+            # Also, if recurse if false it means we're doing an alias, and we already went through season deduction, so do only if true
+            season = malwrapper.deduce_season(showtitle, season)
             parameters['season'] = season
             
     
@@ -177,9 +344,7 @@ def trySearch(parameters, t, the_mal_entry, recurse = True):
             # we try to find aliases
             WebExtractor.log("finding aliases")
             
-            if the_mal_entry == "notfetched":
-                the_mal_entry = get_mal_entry(showtitle)
-            aliases = mal_get_other_titles(the_mal_entry)
+            aliases = malwrapper.get_other_titles(showtitle)
             if aliases:
                 for alias in aliases:
                     if alias == parameters['showtitle']:
@@ -187,7 +352,7 @@ def trySearch(parameters, t, the_mal_entry, recurse = True):
                     WebExtractor.log("trying " + alias)
                     newparameters = parameters
                     newparameters['showtitle'] = alias
-                    searchResults = trySearch(newparameters, t, the_mal_entry, False) # try the alias
+                    searchResults = trySearch(newparameters, t, False) # try the alias
                     if searchResults:
                         return searchResults
             
@@ -217,47 +382,6 @@ def getEpisodeDetails(episode):
                 url = fullUrl
                 )
 
-#------------------------------------------------------------------------------
-# helper function that checks a config file for configured alias overrides
-#
-def getConfiguredAlias(showtitle, showseason, showepisode):
-  
-    if showtitle is None or showtitle is '':
-        return showtitle, showseason, showepisode
-    
-    conf = kdecore.KConfig(confFileName)
-    confgroup = kdecore.KConfigGroup(conf, "aliases")
-    rawdata = confgroup.readEntry(showtitle, QtCore.QStringList()).toStringList()
-    if len(rawdata) > 1:
-        if rawdata[1] == "-":
-            return str(rawdata[0]), showseason, showepisode
-        else:
-            alias = rawdata.takeFirst()
-            season, episode = process_seasonrule(rawdata, showseason, showepisode)
-            return str(alias), season, episode
-    # in case there's unicode involved
-    rawdata = confgroup.readEntry(showtitle.decode('utf'), QtCore.QStringList()).toStringList()
-    if len(rawdata) > 1:
-        if rawdata[1] == "-":
-            return str(rawdata[0]), showseason, showepisode
-        else:
-            alias = rawdata.takeFirst()
-            season, episode = process_seasonrule(rawdata, showseason, showepisode)
-            return str(alias), season, episode
-    return showtitle, showseason, showepisode
-
-# Helper function for above
-def process_seasonrule(rules, showseason, showepisode):
-    for rule in rules:
-        components = QtCore.QString(rule).split(ruleSeparator)
-        if components.count() < 2:
-            return str(components[0]), showepisode
-        if showepisode >= str(components[1]) and (components[2] == "-" or showepisode <= str(components[2])) :
-            if components[3] == "1":
-                return str(components[0]), str(int(str(showepisode)) - int(str(components[1])) + 1)
-            else:
-                return str(components[0]), showepisode
-    return showseason, showepisode # no rules matched
 
 #------------------------------------------------------------------------------
 # extracts all information from the given url
@@ -388,122 +512,10 @@ def extractItemFromUri( url, options ):
     WebExtractor.itemResultsJSON( 'tvshow', data_string )
 
 #------------------------------------------------------------------------------
-# These are helper functions that attempt to find anime showtitle aliases using MyAnimeList
-# get_other_titles is the main function of interest here, and simply returns an array of aliases
-# for the supplied query, if found.
-# Much of this code is adapted from malconstrict, a MAL api python wrapper
-
-malapiurl = 'http://mal-api.com'
-the_mal_entry = -1
-
-def raw_search_anime(query):
-    h = httplib2.Http("/tmp/httplib2")
-    resp, content = h.request(malapiurl + '/anime/search?q=' + urllib2.quote(query))
-    if int(resp['status']) != 200:
-        return None
-    return content
-
-def raw_get_anime_details(anime_id):
-    h = httplib2.Http("/tmp/httplib2")
-    resp, content = h.request(malapiurl + '/anime/' + str(anime_id) + '?mine=0')
-
-    if int(resp['status']) == 404:
-        return None
-    return content
-
-# tries to find the "best match" series entry on MAL for given query
-def get_mal_entry(query):
-    min_acceptable_score = 0.7
-    entries = json.loads(raw_search_anime(query))
-    # put some back into finding the best result!
-    details = None
-    while len(entries) > 0:
-        # first we try and pick out the best title match
-        best_score = 0
-        best_match_index = 0
-        for i in range(len(entries)):
-            if match_score(entries[i]['title'], query) > best_score:
-                best_score = match_score(entries[i]['title'], query)
-                best_match_index = i
-        best_match = entries.pop(best_match_index)
-        if (best_match['type'] != "TV"):
-            continue # we ignore non TV-series for now
-        details = json.loads(raw_get_anime_details(best_match['id']))
-        WebExtractor.log("Evaluating "+ details['title'])
-        # then we check if there is at least one alias meeting the minimum score
-        if (match_score(details['title'], query) > min_acceptable_score):
-            break;
-        if 'english' in details['other_titles']:
-            for title in details['other_titles']['english']:
-                if (match_score(title, query) > min_acceptable_score):
-                    break;
-        if 'synonyms' in details['other_titles']:
-            for title in details['other_titles']['synonyms']:
-                if (match_score(title, query) > min_acceptable_score):
-                    break;
-        details = None
-    return details
-
-def mal_get_other_titles(the_mal_entry):
-    details = the_mal_entry
-    result = None
-    if details:
-        result = [details['title']]
-        if 'english' in details['other_titles']:
-            for title in details['other_titles']['english']:
-                result.append(title)
-        if 'synonyms' in details['other_titles']:
-            for title in details['other_titles']['synonyms']:
-                result.append(title)
-    return result
-
-def mal_deduce_season(the_mal_entry, default):
-    details = the_mal_entry
-    if details:
-        WebExtractor.log("Deducing season number with MAL..")
-        # firstly, a lot of shows list sequel shows as "<showname> Nth Season"
-        # in the synonym list, we look for that, and assume that "Nth Season"
-        # isn't actually part of the real title
-        # TODO: think harder about this assumption, can we avoid it?
-        if 'synonyms' in details['other_titles']:
-            for title in details['other_titles']['synonyms']:
-                if title[-6:].lower() == "season":
-                    if title[-10:-9] == "1" and title[-9:-7].lower() == "st":
-                        WebExtractor.log("Deduced season with synonym " + title)
-                        return 1
-                    elif title[-10:-9] == "2" and title[-9:-7].lower() == "nd":
-                        WebExtractor.log("Deduced season with synonym " + title)
-                        return 2
-                    elif title[-10:-9].isdigit() and title[-9:-7].lower() == "th":
-                        WebExtractor.log("Deduced season with synonym " + title)
-                        return int(title[-10:-9])
-        # well that didn't work, guess we need to do it the hard way.
-        # basically, we see if a prequel is specified, and if it is, we look
-        # and see if the prequel has a prequel, and we deduce the season by
-        # counting the length of the prequel chain, assuming the end of the chain is
-        # season 1
-        season_count = 0
-        curentry = details
-        while True:
-            if curentry['type'] == "TV":
-                WebExtractor.log("Prequel count at " + curentry['title'] + " : " + str(season_count))
-                season_count += 1
-            if len(curentry['prequels']) < 1:
-                break
-            curentry = json.loads(raw_get_anime_details(curentry['prequels'][0]['anime_id']))
-        if season_count > 1:
-            return season_count
-    return default
-
-def match_score(str1, str2):
-    return difflib.SequenceMatcher(None, str1.lower(), str2.lower()).ratio()
-
-#------------------------------------------------------------------------------
 # Code for plugin-specific config dialog.
 # Just a simple interface to add and delete custom aliases.
 # Dealing with uic from python is pretty ill-documented, so not sure
 # how good the methods here are, but they work well enough.
-ruleSeparator = "|"
 class ConfDialogController:
     def __init__(self):
         self.ui = uic.loadUi(uifileFolder + "/tvdbmalconfig.ui", QtGui.QDialog())
